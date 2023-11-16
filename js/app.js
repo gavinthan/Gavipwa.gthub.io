6422e658f066944a5a6b41202e65d12a8e3b2107
gavinthan <83983831+gavinthan@users.noreply.github.com>
2023-11-16 16:24:18 +0100

    Update app.js offline storage

--- js/app.js
+++ js/app.js
@@ -7,6 +7,70 @@ if ("serviceWorker" in navigator) {
      .register("./serviceWorker.js",{ scope: "./" })
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
+
+// Offline storage
+
+    if ('localStorage' in window || 'sessionStorage' in window) {
+  var selectedEngine;
+
+  var logTarget = document.getElementById('target');
+  var valueInput = document.getElementById('value');
+
+  var reloadInputValue = function () {
+  console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
+    valueInput.value = window[selectedEngine].getItem('myKey') || '';
+  }
+  
+  var selectEngine = function (engine) {
+    selectedEngine = engine;
+    reloadInputValue();
+  };
+
+  function handleChange(change) {
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newState = document.createElement('p');
+    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
+    logTarget.appendChild(newState);
+  }
+
+  var radios = document.querySelectorAll('#selectEngine input');
+  for (var i = 0; i < radios.length; ++i) {
+    radios[i].addEventListener('change', function () {
+      selectEngine(this.value)
+    });
+  }
+  
+  selectEngine('localStorage');
+
+  valueInput.addEventListener('keyup', function () {
+    window[selectedEngine].setItem('myKey', this.value);
+  });
+
+  var onStorageChanged = function (change) {
+    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
+    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
+    if (engine === selectedEngine) {
+      reloadInputValue();
+    }
+  }
+
+  window.addEventListener('storage', onStorageChanged);
+}
+HTML
+<p>
+  <label>Engine</label>
+</p>
+<div role="group" id="selectEngine">
+  <input type="radio" name="engine" value="localStorage" checked/> Persistent Storage
+  <input type="radio" name="engine" value="sessionStorage"/> Per-Session Storage
+</div>
+
+<p>
+  <label for="value">Value for <code>myKey</code></label>
+  <input type="text" id="value">
+
+
+    
   });
 }
 
