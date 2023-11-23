b4560b910184d6a746a93a01cc63ff67f1522639
gavinthan <83983831+gavinthan@users.noreply.github.com>
2023-11-23 16:25:11 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,6 +23,21 @@
 
 /// file Access
 
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+      
+
+
+
+      
       function getReadFile(reader, i) {
   return function () {
     var li = document.querySelector('[data-idx="' + i + '"]');
